993e825ff06aacb9bd5567ace1659c77d9a9ecc6
Unknown <luizfilipemonge@outlook.com>
2018-05-04 21:55:14 -0300

    Animações e Fundo

---
+++ LCD-Game/Assets/Scripts/Enviroment/AnimatedBg.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AnimatedBg : MonoBehaviour {
+
+	public float animSpeedX = 0.2f, animSpeedY = 0.4f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		Vector2 offset = new Vector2 (Time.time * animSpeedX, Time.time * animSpeedY);
+		GetComponent<Renderer>().material.mainTextureOffset = offset;
+	}
+}
